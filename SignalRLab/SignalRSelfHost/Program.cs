2bbd82cd316903e3087d87bdfde8a90928dcfb89
Kooboo-youcai <youcai@kooboo.com>
2013-12-18 16:23:05 +0800

    Add SignalR sample

---
+++ SignalRLab/SignalRSelfHost/Program.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Microsoft.AspNet.SignalR;
+using Microsoft.Owin.Cors;
+using Microsoft.Owin.Hosting;
+using Owin;
+
+namespace SignalRSelfHost
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string url = "Http://localhost:8080";
+            using (WebApp.Start(url))
+            {
+                Console.WriteLine("Server is running on {0}",url);
+                Console.ReadLine();
+            }
+        }
+    }
+
+    class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            app.UseCors(CorsOptions.AllowAll);
+            app.MapSignalR();
+        }
+    }
+
+    public class MyHub : Hub
+    {
+        public void Send(string name, string message)
+        {
+            Clients.All.addmessage(name,message);
+        }
+    }
+}
